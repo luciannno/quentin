67837e6484deab725d3ef6cfd73b1792afb3051f
lbrunette <lbrunette@servidor.(none)>
2017-08-05 17:22:14 -0300

    First Project Structure

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='quentin',
+    version='0.1.0',
+    description='Another python quantitative analysis software',
+    long_description=readme,
+    author='Luciano Brunette',
+    author_email='luciano@brunette.me.uk',
+    url='https://github.com/luciano/quentin',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
